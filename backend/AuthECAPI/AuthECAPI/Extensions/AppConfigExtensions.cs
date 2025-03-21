f4769327ca2f1be152c875a94241b15a3dedc45e
Badal-Prakash <lav14251@gmail.com>
2025-03-21 22:01:05 +0530

    backend pushed

---
+++ backend/AuthECAPI/AuthECAPI/Extensions/AppConfigExtensions.cs
@@ -0,0 +1,26 @@
+﻿using AuthECAPI.Models;
+
+namespace AuthECAPI.Extensions
+{
+  public static class AppConfigExtensions
+  {
+    public static WebApplication ConfigureCORS(
+        this WebApplication app,
+        IConfiguration config)
+    {
+      app.UseCors(options =>
+      options.AllowAnyOrigin()
+      .AllowAnyMethod()
+      .AllowAnyHeader());
+      return app;
+    }
+
+    public static IServiceCollection AddAppConfig(
+        this IServiceCollection services,
+        IConfiguration config)
+    {
+      services.Configure<AppSettings>(config.GetSection("AppSettings"));
+      return services;
+    }
+  }
+}
---
+++ backend/AuthECAPI/AuthECAPI/Program.cs
@@ -0,0 +1,32 @@
+using AuthECAPI.Controllers;
+using AuthECAPI.Extensions;
+using AuthECAPI.Models;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddSwaggerExplorer()
+                .InjectDbContext(builder.Configuration)
+                .AddAppConfig(builder.Configuration)
+                .AddIdentityHandlersAndStores()
+                .ConfigureIdentityOptions()
+                .AddIdentityAuth(builder.Configuration);
+
+var app = builder.Build();
+
+app.ConfigureSwaggerExplorer()
+   .ConfigureCORS(builder.Configuration)
+   .AddIdentityAuthMiddlewares();
+
+app.MapControllers();
+app.MapGroup("/api")
+   .MapIdentityApi<AppUser>();
+app.MapGroup("/api")
+   .MapIdentityUserEndpoints()
+   .MapAccountEndpoints()
+   .MapAuthorizationDemoEndpoints();
+app.Run();
+
+
+
